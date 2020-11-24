894bae470d9fd1c8596539ff81c9c1263a39d14b
Jorge Gallego <59119768+Jorge644240@users.noreply.github.com>
2020-11-24 12:40:01 -0500

    Update visulizacion.py
    Horario implementado

--- visulizacion.py
+++ visulizacion.py
@@ -6,21 +6,33 @@ from matplotlib.offsetbox import AnnotationBbox
 
 print("Listo")
 
-fig, axes = plt.subplots()
-axes.get_xaxis().set_visible(False)
-axes.get_yaxis().set_visible(False)
+def horario(modelo):
+    colores = {"Cálculo": "red", "Algoritmos": "blue", "Fundamentos": "green", "Análisis": "grey", "Cívica": "yellow", "Lógica": "purple"}
+    
+    fig, axes = plt.subplots()
+    axes.get_xaxis().set_visible(False)
+    axes.get_yaxis().set_visible(False)
 
-horizontal_step = 1./7
-vertical_step = 1./6
-tangulos = list()
+    horizontal_step = 1./7
+    vertical_step = 1./6
+    tangulos = list()
 
-for i in range(7):
-    tangulos.append(patches.Rectangle((i*horizontal_step, 3*vertical_step), horizontal_step, vertical_step, facecolor='black'))
+    for i in range(7):
+        tangulos.append(patches.Rectangle((i*horizontal_step, 3*vertical_step), horizontal_step, vertical_step, facecolor='black'))
+        
+    for key in modelo.keys():
+        tangulos.append(patches.Rectangle((key[1]*horizontal_step, (key[2]+1)*vertical_step), horizontal_step, vertical_step, facecolor=colores[key[0]]))
 
-for t in tangulos:
-    axes.add_patch(t)
+    for t in tangulos:
+        axes.add_patch(t)
 
-fig.savefig("horario1.png")
+    fig.savefig("horario1.png")
+    
+modelo = {("Algoritmos", 0, 1): 1, ("Fundamentos", 0, 3): 1, ("Lógica", 1, 0): 1, ("Análisis", 2, 0): 1, ("Fundamentos", 2, 3): 1, \
+          ("Cálculo", 2, 4): 1, ("Lógica", 3, 0): 1, ("Algoritmos", 3, 3): 1, ("Cálculo", 4, 0): 1, ("Cívica", 4, 1): 1, \
+          ("Cálculo", 4, 4): 1, ("Algoritmos", 5, 1): 1}
+
+horario(modelo)
 """
 def tablero(f, n):
     days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"]
