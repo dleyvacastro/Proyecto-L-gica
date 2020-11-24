c02437efbb90f12d42daeaab5b8b6264662f3876
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2020-11-24 12:25:39 -0500

    Update Reglas.py

--- Reglas.py
+++ Reglas.py
@@ -86,27 +86,25 @@ def regla1():
 def regla2():
     inicial = 1
     for d in range(6):
-        
         for m3 in range(7):
-            for d3 in range(6):
-                for h3 in range(6):
-                    for m2 in range(7):
-                        for d2 in range(6):
-                            for h2 in range(6):
-                                for m1 in range(7):
-                                    for d1 in range(6):
-                                        for h1 in range(6):
-                                            if inicial:
-                                                if (m1!=m2 or d1 != d2 or h1 != h2) and (m1!=m3 or d1 != d3 or h1 != h3) and (m2!=m3 or d2 != d3 or h2 != h3):
-                                                    formula = P(m3, d3, h3, Nmaterias, Ndias, Nhoras) + P(m2, d2, h2, Nmaterias, Ndias, Nhoras) + 'Y'
-                                                    formula += P(m1, d1, h1, Nmaterias, Ndias, Nhoras) + 'Y'
-                                                    inicial = 0
-                                            else:
-                                                if (m1!=m2 or d1 != d2 or h1 != h2) and (m1!=m3 or d1 != d3 or h1 != h3) and (m2!=m3 or d2 != d3 or h2 != h3):
-                                                     formula += P(m3, d3, h3, Nmaterias, Ndias, Nhoras) + P(m2, d2, h2, Nmaterias, Ndias, Nhoras) + 'Y'
-                                                     formula += P(m1, d1, h1, Nmaterias, Ndias, Nhoras) + 'Y' +'O'
+            for h3 in range(6):
+                for m2 in range(7):
+                    for h2 in range(6):
+                        for m1 in range(7):
+                            for h1 in range(6):
+                                if inicial:
+                                    if(m1 != m2 or h1 != h2) and (m1 != m3 or h1 != h3) and (m2 != m3 or h2 != h3):
+                                        formula = P(m3, d, h3, Nmaterias, Ndias, Nhoras) + P(m2, d, h2, Nmaterias, Ndias, Nhoras) + 'Y'
+                                        formula += P(m1, d, h1, Nmaterias, Ndias, Nhoras) + 'Y'
+                                        inicial = 0
+                                else :
+                                
+                                    if(m1 != m2 or h1 != h2) and (m1 != m3 or h1 != h3) and (m2 != m3 or h2 != h3):
+                                        formula += P(m3, d, h3, Nmaterias, Ndias, Nhoras) + P(m2, d, h2, Nmaterias, Ndias, Nhoras) + 'Y'
+                                        formula += P(m1, d, h1, Nmaterias, Ndias, Nhoras) + 'Y' + 'O'
+
     return formula
-#print(len(regla2()))
+#print(regla2())
 #-------------------------------------Regla 2-----------------------------------------------------#
 def regla3():
     inicial = 1
@@ -116,7 +114,7 @@ def regla3():
                 formula = P(m, d, 2, Nmaterias, Ndias, Nhoras)
                 inicial = 0
             else:
-                formula += P(m, d, 3, Nmaterias, Ndias, Nhoras) + 'O'
+                formula += P(m, d, 2, Nmaterias, Ndias, Nhoras) + 'O'
     formula += '-'
     return formula
 #-------------------------------------Regla 4-----------------------------------------------------#
@@ -137,14 +135,13 @@ def regla4(i):
 
 #--------------------------------------------------------------------------------------------------#
 
-print("regla 1: ")    
-print(Inorderp(String2Tree(regla1())))
-print("regla 2:")
-#print(Inorderp(String2Tree(regla2())))
-print("regla 3: ")
-print(Inorderp(String2Tree(regla3())))
-print("regla 4: ")
-for i in range(7):
-    print("regla 4.{0}: ".format(i))
-    print(Inorderp(String2Tree(regla4(i))))
-    
+# print("regla 1: ")    
+# #print(Inorderp(String2Tree(regla1())))
+# print("regla 2:")
+# print(Inorderp(String2Tree(regla2())))
+# print("regla 3: ")
+# #print(Inorderp(String2Tree(regla3())))
+# print("regla 4: ")
+# for i in range(7):
+#     print("regla 4.{0}: ".format(i))
+# #    print(Inorderp(String2Tree(regla4(i))))
