6e41e677e20d7d625caf147d68bd6ac33739e12e
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2020-11-25 13:20:17 -0500

    Update visulizacion.py

--- visulizacion.py
+++ visulizacion.py
@@ -3,11 +3,12 @@ matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.offsetbox import AnnotationBbox
+from modelaje import modelo
 
 print("Listo")
 
 def horario(modelo):
-    colores = {"Cálculo": "red", "Algoritmos": "blue", "Fundamentos": "green", "Análisis": "grey", "Cívica": "yellow", "Lógica": "purple"}
+    colores = {0: "red", 1: "blue", 2: "green", 3: "grey", 4: "yellow", 5: "purple"}
     
     fig, axes = plt.subplots()
     axes.get_xaxis().set_visible(False)
@@ -28,40 +29,6 @@ def horario(modelo):
 
     fig.savefig("horario1.png")
     
-modelo = {("Algoritmos", 0, 1): 1, ("Fundamentos", 0, 3): 1, ("Lógica", 1, 0): 1, ("Análisis", 2, 0): 1, ("Fundamentos", 2, 3): 1, \
-          ("Cálculo", 2, 4): 1, ("Lógica", 3, 0): 1, ("Algoritmos", 3, 3): 1, ("Cálculo", 4, 0): 1, ("Cívica", 4, 1): 1, \
-          ("Cálculo", 4, 4): 1, ("Algoritmos", 5, 1): 1}
-
-horario(modelo)
-"""
-def tablero(f, n):
-    days = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sábado", "Domingo"]
-    colors = {"Calculo": "red", "Fundamentos": "blue", "Algoritmos": "green", "Lógica": "black", "Cívica": "grey", "Argumentos": "purple", "Electiva": "orange"}
-    time = {7:1, 8:2, 9:3, 10:4, 11:5, 12:6, 13:7, 14:8, 15:9, 16:10, 17:11, 18:12}
-
-    fig, axes = plt.subplots()
-    axes.get_xaxis().set_visible(False)
-    axes.get_yaxis().set_visible(False)
-
-    vertical = 1./13
-    horizontal = 1./7
-    count = 0
-    tangulos = []
-
-    for i in range(13):
-        for l in range(7):
-            tangulos.append(patches.Rectangle((i*horizontal, (j+1)*vertical), horizontal, vertical, angle=0.0, facecolor="transparent")
-
-    for i in range(7):
-        ver = i*horizontal
-        tangulos.append(patches.Rectangle((0, ver), 0.005, 1, angle=0.0, facecolor="black"))
-
-    for i in range(13):
-        hor = i*horizontal
-        tangulos.append(patches.Rectangle((hor, 0), 1, 0.005, angle=0.0, facecolor="black"))
-
-    for t in tangulos:
-        axes.add_patch(t)
-
-    fig.savefig("horario_"+n+".png")
-"""
+# modelo = {("Algoritmos", 0, 1): 1, ("Fundamentos", 0, 3): 1, ("Lógica", 1, 0): 1, ("Análisis", 2, 0): 1, ("Fundamentos", 2, 3): 1, \
+#           ("Cálculo", 2, 4): 1, ("Lógica", 3, 0): 1, ("Algoritmos", 3, 3): 1, ("Cálculo", 4, 0): 1, ("Cívica", 4, 1): 1, \
+#           ("Cálculo", 4, 4): 1, ("Algoritmos", 5, 1): 1}
