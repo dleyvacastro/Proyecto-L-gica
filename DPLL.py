91e11cb56dc5e3d2d4cbeebdce45092a5cc3aec5
dleyvacastro <62482485+dleyvacastro@users.noreply.github.com>
2020-11-25 13:16:52 -0500

    Update DPLL.py

--- DPLL.py
+++ DPLL.py
@@ -1,25 +1,24 @@
-LP = [chr(x) for x in range(97, 123)]
-print(LP)
+#from Letras import letras
 
-def hay_unidad(s):
+LP = [chr(x) for x in range(256, 20000)]
+def Unidad(s):
     for i in s:
         if len(i) == 1:
             return i[0]
-    return None
 
-def complemento(l):
+def Complemento(l):
     if l in LP:
         return '-' + l
-    elif l[0] == '-':
+    elif l[1] in LP:
         return l[1]
     else:
         my_error = ValueError("ERROR")
         raise my_error
 
 def unit_Propagate(s, i):
-    unidad = hay_unidad(s)
+    unidad = Unidad(s)
     while ([] not in s) and (unidad != None):
-        com = complemento(unidad)
+        com = Complemento(unidad)
         s = [c for c in s if unidad not in c]
         for c in s:
             if com in c:
@@ -28,21 +27,20 @@ def unit_Propagate(s, i):
             i[com] = 0
         else:
             i[unidad] = 1
-        unidad = hay_unidad(s)
+        unidad = Unidad(s)
     return s, i
 
 
 def DPLL(s, i):
-    y = []
     s, i = unit_Propagate(s, i)
-    if y in s:
+    if [] in s:
         return False, {}
     elif len(s) == 0:
         return True, i
     else:
         
         l = s[0][0]
-        com = complemento(l)
+        com = Complemento(l)
         s2 = [j[:] for j in s if l not in j]
         for x in s2:
             if com in x:
@@ -53,8 +51,8 @@ def DPLL(s, i):
         else:
             i2[l] = 1
             
-        a, b = DPLL(s2, i2)
-        if a == True:
+        x, y = DPLL(s2, i2)
+        if x == True:
             return True, i2
         else:
             s3 = [x[:] for x in s if com not in x]
@@ -67,15 +65,12 @@ def DPLL(s, i):
             else:
                 i3[l] = 0
             return DPLL(s3, i3)
-        
 
-a = [['p','q','r'], ['-p', '-q', '-r'], ['-p', 'q', 'r'], ['-q', 'r'], ['q', '-r']]
-b = [['p','q','r','-s'],['p','t','s'],['-p','-q'],['p','r','-q','-s']]
-c = [['p','q','-r'],['r','s','t'],['t'],['p','s'],['q','-p']]
-d = [['p','-q'], ['-p','-q'],['q','r'],['-q','-r'],['-p','-r'],['p','-r']]
-e = [['r','p','s'],['-r','-p','s'],['-r','p','s'],['p','-s']]
+"""
 sol ={}
-x, y = DPLL(e, sol)
+x, y = DPLL(R1, sol)
 print(x)
 if x:
     print(y)
+    """
+    
