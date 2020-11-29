003822c3ff370ba5f4a1111b206ffdf1097fcd81
Jorge Gallego <59119768+Jorge644240@users.noreply.github.com>
2020-11-29 11:07:10 -0500

    Update reglas.py
    Reglas actualizadas

--- reglas.py
+++ reglas.py
@@ -1,48 +1,64 @@
+"""
 # Las clases no se pueden cruzar
 def regla1(modelo):
+    modelo_n = []
+    new_modelo = {}
     for key in modelo.keys():
         dia = key[1]
         hora = key[2]
-        for other in modelo.keys():
-            if other != key and key[1]==other[1] and key[2]==other[2]:
-                modelo.pop(other)
-    return modelo
+        if (dia, hora) not in modelo_n:
+            modelo_n.append((dia, hora))
+            new_modelo[key] = modelo[key]
+    return new_modelo
+"""
+from random import randint
 
 # Máximo tres clases por dia
 def regla2(modelo):
-    dias = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
-    modelo_n = {}
+    dias = {0: [], 1: [], 2: [], 3: [], 4: [], 5: []}
     for key in modelo.keys():
-        if dias[key[1]]<3:
-            modelo_n[key] = 1
-            dias[key[1]] += 1
-        else:
-            for comp in modelo.keys():
-                if key!=comp:
-                    modelo.pop(comp)
+        dias[key[1]].append(key)
+    #print(dias[0])
+    del modelo
+    modelo = {}
+    for key in dias.keys():
+        length = dias[key].__len__()
+        one, two, three = randint(0, length-2), randint(0, length-2), randint(0, length-2)
+        while one==two or one==three or two==three or dias[key][one][2]==dias[key][two][2] or dias[key][two][2]==dias[key][three][2] or \
+        dias[key][one][2]==dias[key][three][2] or dias[key][one][0]==dias[key][two][0] or dias[key][one][0]==dias[key][three][0] or \
+        dias[key][two][0]==dias[key][three][0]:
+            one, two, three = randint(0, length-2), randint(0, length-2), randint(0, length-2)
+        first, second, third = dias[key][one], dias[key][two], dias[key][three]
+        dias[key] = [first, second, third]
+    for value in dias.values():
+        for element in value:
+            modelo[element] = 1
     return modelo
+    """
+        if dias[key[1]].__len__()<3:
+    for key in modelo.keys():
+            dias[key[1]].append(key)
+    del modelo
+    modelo = {}
+    for key in dias.keys():
+        for element in dias[key]:
+            modelo[element] = 1
+    return modelo"""
 
 # NO hay clase a las 12
 def regla3(modelo):
+    almuerzo = []
     for key in modelo.keys():
-        if key[2]==2:
-            modelo.pop(key)
+        if key[2]==3:
+            almuerzo.append(key)
+    for element in almuerzo:
+        modelo.pop(element)
     return modelo
-
+"""
 # Solo dos sesiones de cada materia por semana
 def regla4(modelo):
     clases = set()
-    for key in modelo.keys():
-        clases.add(key[0])
-    semana = {}
-    for element in clases:
-        semana[element] = 0
-    del clases
-    for key in modelo.keys():
-        if semana[key[0]]<2:
-            semana[key[0]] += 1
-        else:
-            for comp in modelo.keys():
-                if key!=comp:
-                    modelo.pop(comp)
-    return modelo
+    for keys in modelo.keys():
+        clases.add(keys[0])
+    print(clases)
+"""
